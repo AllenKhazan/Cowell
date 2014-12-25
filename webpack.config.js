d5ef1bddb44bc62a2876276de8e8dd785fb645d3
Rich Seymour <rseymour@gmail.com>
2014-12-24 23:12:18 -0500

    getting things working w/ jsx & react

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './modules/main.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ["", ".jsx", ".js"],
+    modulesDirectories: ["modules", "node_modules"]
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loader: 'jsx-loader?harmony' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
+    ]
+  }
+};
