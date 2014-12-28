f5b1d4bb6174e20d890ce210c5b5e3b638aebaa2
Rich Seymour <rseymour@gmail.com>
2014-12-28 12:42:32 -0500

    buttons

--- modules/cowell.jsx
+++ modules/cowell.jsx
@@ -1,7 +1,29 @@
 var React = require('react');
+var Button = React.createClass({
+  propTypes: {
+    color: React.PropTypes.string.isRequired
+  },
+  _onClick: function(t) {
+    console.log("clicked", this.props.color);
+  },
+  render: function() {
+    var style = { background: this.props.color,
+                  height: '100px', 
+                  width: '100px',
+                  border: '2px solid black' };
+    return( <div onClick={this._onClick} style={style} ></div>)
+  }
+});
 var Cowell = React.createClass({
   render: function() {
-    return ( <h1>HI!</h1>);
+    return (
+      <div>
+        <Button color="red"/>
+        <Button color="green"/>
+        <Button color="blue"/>
+        <Button color="yellow"/>
+      </div>
+    );
   }
 });
 
